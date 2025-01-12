4bfc8fcbae4595bc1388e6ee13e4ee58e6c7818e
foy4748 <faisaljfcl@gmail.com>
2025-01-13 02:59:48 +0600

    Post SocialLogin Data on MongoDB

--- src/app/login/components/SocialLogin.jsx
+++ src/app/login/components/SocialLogin.jsx
@@ -1,24 +1,25 @@
 "use client";
 import { FaGithub } from "react-icons/fa6";
 import { FaGoogle } from "react-icons/fa";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { useEffect } from "react";
 export default function SocialLogin() {
   const router = useRouter();
+  const session = useSession();
 
-  const handleSocialLogin = async (providerName) => {
-    console.log("SOCIAL LOGIN", providerName);
-    const result = await signIn(providerName, { redirect: false });
-    console.log(result);
-    if (result.ok) {
-      router.push("/");
-      toast.success(`Logged In Successfully using ${providerName}`);
-    } else {
-      toast.error("Something Went Wrong");
-    }
-    console.log(result);
+  const handleSocialLogin = (providerName) => {
+    signIn(providerName);
   };
+
+  useEffect(() => {
+    if (session?.status == "authenticated") {
+      router.push("/");
+      toast("Successfully Logged IN");
+    }
+  }, [session?.status]);
+
   return (
     <div className="flex justify-center gap-8">
       <p
